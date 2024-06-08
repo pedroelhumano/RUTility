5d7b71be7a44748ba9784474ef6486ebf9380571
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2024-06-08 16:43:13 -0400

    optimization of fn formatValidation

--- src/Utils/formatValidation.ts
+++ src/Utils/formatValidation.ts
@@ -7,19 +7,10 @@ export const formatValidations = (rut: string): void => {
         throw new Error("Invalid RUT format. RUT cannot start with zero.");
     }
 
-    let rutDigits = rut.replace(/\./g, '');
-
-    if (rut.includes('-')) {
-        rutDigits = rutDigits.replace(/-/g, '');
-        if (rutDigits.length !== 9) {
-            throw new Error("Invalid RUT format. RUT must have at least 9 characters if the RUT contains the '-' character.");
-        }
-    } else if (rutDigits.length !== 8) {
-        throw new Error("Invalid RUT format. RUT must have at least 8 characters.");
-    }
-
-    const numericRegexRut = /^\d{8}[\dkK]?$/;
-    if (!numericRegexRut.test(rutDigits)) {
-        throw new Error('Invalid RUT format. RUT must have at least 8 numeric characters or 9 if it contains a "-" character.');
+    const cleanRut = rut.replace(/[\.\-]/g, '');
+    
+    const numericRegexRut = /^\d{1,10}$/;
+    if (!numericRegexRut.test(cleanRut)) {
+        throw new Error('Invalid RUT format. RUT must be numeric and have between 1 and 10 digits.');
     }
 };
