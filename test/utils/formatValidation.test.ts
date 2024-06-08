ba2f615e0bbd09513243652b3726c02681fcbf01
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2024-06-08 16:43:28 -0400

    unit test formatValidation

--- test/utils/formatValidation.test.ts
+++ test/utils/formatValidation.test.ts
@@ -1,4 +1,4 @@
-import {describe, test, expect} from '@jest/globals';
+import { describe, test, expect } from '@jest/globals';
 import { formatValidations } from '../../src/Utils/formatValidation';
 
 describe('formatValidations function', () => {
@@ -15,33 +15,33 @@ describe('formatValidations function', () => {
         }).toThrowError("Invalid RUT format. RUT cannot start with zero.");
     });
 
-    test('should throw error if the RUT length is less than 8 characters', () => {
+    test('should throw error if the RUT has more than 10 digits', () => {
         expect(() => {
-            formatValidations('1234567');
-        }).toThrowError("Invalid RUT format. RUT must have at least 8 characters.");
+            formatValidations('12345678901');
+        }).toThrowError("Invalid RUT format. RUT must be numeric and have between 1 and 10 digits.");
     });
-
-    test('should throw error if the RUT contains "-" and has less than 9 characters', () => {
-        expect(() => {
-            formatValidations('12.345.67-');
-        }).toThrowError("Invalid RUT format. RUT must have at least 9 characters if the RUT contains the '-' character.");
-    });
-
-    // test('should throw error if the RUT contains non-numeric characters', () => {
-    //     expect(() => {
-    //         formatValidations('12.34a.678-9');
-    //     }).toThrowError("Invalid RUT format. RUT must have at least 8 numeric characters or 9 if it contains a '-' character.");
-    // });
 
     test('should not throw error for a valid RUT without "-"', () => {
         expect(() => {
             formatValidations('12345678');
         }).not.toThrow();
+        expect(() => {
+            formatValidations('543');
+        }).not.toThrow();
+    });
+
+    test('should throw error if the RUT contains non-numeric characters', () => {
+        expect(() => {
+            formatValidations('12.34a.678');
+        }).toThrowError('Invalid RUT format. RUT must be numeric and have between 1 and 10 digits.');
     });
 
     test('should not throw error for a valid RUT with "-"', () => {
         expect(() => {
             formatValidations('12.345.678-9');
+        }).not.toThrow();
+        expect(() => {
+            formatValidations('2.345.678-9');
         }).not.toThrow();
     });
 });
