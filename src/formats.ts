087c1a4ab1965355deab1bda2f7462c64a992948
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2024-06-09 11:43:15 -0400

    update dotDash fn

--- src/formats.ts
+++ src/formats.ts
@@ -1,5 +1,7 @@
 import { formatValidations } from "./Utils/formatValidation";
 
+const removeDotsAndDashes = (rut: string): string => rut.replace(/[.-]/g, '');
+
 export const format = {
     dot: (rut: string): string => {
         formatValidations(rut);
@@ -19,36 +21,18 @@ export const format = {
 
     dotDash: (rut: string): string => {
         formatValidations(rut);
-        
-        // Eliminar puntos y guiones para trabajar con el RUT limpio
-        const cleanRut = rut.replace(/\./g, '').replace(/-/g, '');
+        const cleanRut = removeDotsAndDashes(rut);
     
-        // Caso especial para RUTs de dos dígitos
         if (cleanRut.length === 2) {
             return `${cleanRut[0]}-${cleanRut[1]}`;
         }
-    
-        // Separar el dígito verificador
+
         const dv = cleanRut.slice(-1);
         const rutWithoutDv = cleanRut.slice(0, -1);
-    
-        // Formatear el RUT sin dígito verificador
         const formattedRut = rutWithoutDv.replace(/\B(?=(\d{3})+(?!\d))/g, '.');
     
-        // Verificar si el RUT ya está correctamente formateado
-        const alreadyFormatted = rut.includes('.') && rut.includes('-');
-    
-        if (alreadyFormatted) {
-            return rut; // No hacer nada si ya está correctamente formateado
-        } else if (rut.includes('.')) {
-            return `${formattedRut}-${dv}`; // Solo agregar guion si ya tiene puntos
-        } else if (rut.includes('-')) {
-            return formattedRut + '-' + dv; // Solo agregar puntos si ya tiene guion
-        } else {
-            return `${formattedRut}-${dv}`; // Agregar ambos si no tiene ni puntos ni guion
-        }
+        return `${formattedRut}-${dv}`;
     },
-    
 
     notDot: (rut: string): string => {
         formatValidations(rut);
